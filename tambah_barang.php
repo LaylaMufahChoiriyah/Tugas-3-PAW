785cf565d88ede2f1f339c020a9d43694ae9322c
LaylaMufahChoiriyah <layla.mfh25@gmail.com>
2022-04-04 19:19:36 +0700

    menambahkan file tambah_barang.php

---
+++ tambah_barang.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Tambah Barang</title>
+</head>
+<body>
+	<h1>Form Tambah Barang</h1>
+	<form method="post" action="action.php">
+			<table border="1">
+				<tr>
+					<td><label for="Kode">Kode Barang : </label></td>
+					<td><input id="kode" name="kode" type="text"/></td>
+				</tr>
+				<tr>
+					<td><label for="nama">Nama Barang : </label></td>
+					<td><input id="nama" name="nama" type="text"/></td>
+				</tr>
+				<tr>
+					<td colspan="2"><center><input type="submit" value="submit"></center></td>
+				</tr>
+			</table>
+	</form>
+</body>
+</html>
